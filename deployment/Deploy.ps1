901a669759d260eb724210eae0b91819f5551b64
Lilin Huang <ryelilinh@gmail.com>
2022-07-26 16:50:56 +0100

    Add checking for Node.js and Git installed

--- deployment/Deploy.ps1
+++ deployment/Deploy.ps1
@@ -19,6 +19,7 @@ process {
         Write-Host "=============================================================`n`n"
     }
 
+    # Checking Azure CLI is installed
     $AzCliCheck= (az version | ConvertFrom-Json | Select -ExpandProperty "azure-cli")
     if( $AzCliCheck -eq $null){
         Write-Host "Azure CLI is not installed and please go to this link to install. (https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest%3FWT.mc_id%3Dlearnlti-github-cxa)"        
@@ -27,6 +28,7 @@ process {
     else {
         Write-Host "Azure CLI is installed."
     }
+    # Checking .Net Framework is installed
     $NetFrCheck= (dotnet --list-sdks)
     if( $NetFrCheck -eq $null){
         Write-Host ".Net Framework is not installed and please go to this link to install. (https://dotnet.microsoft.com/en-us/download/dotnet/3.1?WT.mc_id=learnlti-github-cxa)"        
@@ -34,6 +36,24 @@ process {
     }
     else {
         Write-Host ".Net Framework is installed."
+    }
+    # Checking Node.js is installed
+    $NodejsCheck= (node -v)
+    if( $NodejsCheck -eq $null){
+        Write-Host "Node.js is not installed and please go to this link to install. (https://nodejs.org/en/download/)"        
+        exit
+    }
+    else {
+        Write-Host "Node.js is installed."
+    }
+    # Checking Git is installed
+    $GitCheck= (git --version)
+    if( $GitCheck -eq $null){
+        Write-Host "Git is not installed and please go to this link to install. (https://git-scm.com/downloads)"        
+        exit
+    }
+    else {
+        Write-Host "Git is installed."
     }
     
     try {
